813482b7f33d6249b337fc704f1fd259b4d55256
Naveen Kumar Chitturi <naveenkumar200101@gmail.com>
2024-08-25 12:32:47 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react'
 
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dasboard from './pages/Dasboard'
+import Projects from './pages/Projects'
+import NoPageFound from './pages/NoPageFound'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const router = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/about', element: <About /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/sign-up', element: <SignUp /> },
+    { path: '/dashboard', element: <Dasboard /> },
+    { path: '/projects', element: <Projects /> },
+    { path: '*', element: <NoPageFound /> },
+  ])
+
+
   return (
     <>
-      <div className="text-red-400">Hi</div>
+      <RouterProvider router={router} />
 
     </>
   )
