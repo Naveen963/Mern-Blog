072aea676f2429a06791e8ea23bcde028de9e067
Naveen Kumar Chitturi <naveenkumar200101@gmail.com>
2024-09-08 10:54:49 +0530

    Created add posts section in dashboard

--- client/src/pages/Dasboard.jsx
+++ client/src/pages/Dasboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
 
 const Dasboard = () => {
     const location = useLocation();
@@ -18,6 +19,7 @@ const Dasboard = () => {
                 <DashSidebar />
             </div>
             {tab === 'profile' && <DashProfile />}
+            {tab === 'posts' && <DashPosts />}
         </div>
     )
 }
